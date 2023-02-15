902608fe8067b6e8ede8f535e3da496684a59ced
baymau <112998676+khoaLe12@users.noreply.github.com>
2023-02-15 20:29:31 +0700

    update Database

--- WebProject/src/main/java/com/codeweb/configs/WebApplicationContextConfig.java
+++ WebProject/src/main/java/com/codeweb/configs/WebApplicationContextConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -54,5 +55,13 @@ public class WebApplicationContextConfig implements WebMvcConfigurer{
         resource.setSuffix(".jsp");
         
         return resource;
+    }
+    
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+
+        return resolver;
     }
 }
